a32625e13f77e12804e3fd27b8d5ec603b35dbe7
Vishesh Kumar <vishesh.kumar@privado.ai>
2021-06-14 10:07:51 +0530

    added

---
+++ test_1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+int main(){
+    char rs[] = "latinos";
+    float employee_illness_record = 3.9;
+    char dna_record[] = "positive";
+    int retirement_account_information = 0;
+    return 0;
+}
